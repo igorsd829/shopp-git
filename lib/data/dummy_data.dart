0d554c6dfbf01a19f48e7ba1ffcb64f98f2883b6
igorsd829 <igoralexandresd829@gmail.com>
2023-05-16 10:27:08 -0300

    app shopp completo

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,66 @@
+import 'package:shop/models/product.dart';
+
+final dummyProducts = [
+  Product(
+    id: 'p5',
+    name: 'Playstation 5',
+    description:
+        'O PlayStation 5 é um console de jogos eletrônicos de nona geração, desenvolvido pela Sony Interactive Entertainment.',
+    price: 4300,
+    imageUrl:
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/wp6967907-1600321836.png?crop=0.564xw:1.00xh;0.183xw,0&resize=640:*',
+  ),
+  Product(
+    id: 'p6',
+    name: 'Xbox series x',
+    description:
+        'Apresentamos o Xbox Series X, nosso console mais rápido e poderoso de todos os tempos. Jogue milhares de títulos de quatro gerações de consoles- todos os jogos têm melhor aparência e são melhor executados no Xbox Series X.',
+    price: 5370,
+    imageUrl:
+        'https://http2.mlstatic.com/D_NQ_NP_794435-MLA45046407331_032021-O.jpg',
+  ),
+  Product(
+    id: 'p7',
+    name: 'Xbox series s',
+    description:
+        'O PlayStation 5 é um console de jogos eletrônicos de nona geração, desenvolvido pela Sony Interactive Entertainment.',
+    price: 2200,
+    imageUrl:
+        'https://cdn.awsli.com.br/600x450/2443/2443989/produto/17305451073a6412ddc.jpg',
+  ),
+  Product(
+    id: 'p8',
+    name: 'nintendo switch',
+    description:
+        'O PlayStation 5 é um console de jogos eletrônicos de nona geração, desenvolvido pela Sony Interactive Entertainment.',
+    price: 2300,
+    imageUrl:
+        'https://boulanger.scene7.com/is/image/Boulanger/0045496453442_h_f_l_0?wid=500&hei=500',
+  ),
+  Product(
+    id: 'p9',
+    name: 'controle ps5 custom',
+    description:
+        'O PlayStation 5 é um console de jogos eletrônicos de nona geração, desenvolvido pela Sony Interactive Entertainment.',
+    price: 390,
+    imageUrl: 'https://imgs.casasbahia.com.br/55056202/1g.jpg',
+  ),
+  Product(
+    id: 'p10',
+    name: 'controle xbox custom',
+    description:
+        'O PlayStation 5 é um console de jogos eletrônicos de nona geração, desenvolvido pela Sony Interactive Entertainment.',
+    price: 420,
+    imageUrl:
+        'https://assets.xboxservices.com/assets/8e/d4/8ed4fb28-7a81-4abe-94e4-c5327b25e697.jpg?n=Accessories-Hub_Content-Placement-0_2020-Controller-Blue_788x444.jpg',
+  ),
+  Product(
+    id: 'p11',
+    name: 'controle nintendo switch custom',
+    description:
+        'O PlayStation 5 é um console de jogos eletrônicos de nona geração, desenvolvido pela Sony Interactive Entertainment.',
+    price: 349,
+    imageUrl:
+        'https://i5.walmartimages.com/asr/d369ca54-ae23-4029-8efd-06f3e51fd443.794df786f42f3823782d03d0884702e5.jpeg',
+  ),
+];
